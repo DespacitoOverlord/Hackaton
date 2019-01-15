b1dfb6405a0514f436e1e6876c9b759c471ce976
Bigger Jerry <42815148+DespacitoOverlord@users.noreply.github.com>
2019-01-15 09:19:39 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+$(".family").click(function(){
+
+    $(".family2").text("It decreses the chance of you communicating with your family as you are more focused on video games"); 
+ 
+});
+
+$(".social").click(function(){
+
+    $(".social2").text("You lose your social life by gaming disorders and you feel outcasted from society"); 
+ 
+});
+
+$(".family").click(function(){
+
+    $(".family2").text("It decreses the chance of you communicating with your family"); 
+ 
+});
+
+$(".personal").click(function(){
+
+    $(".personal2").text("You spend all of your time gaming because of your disorder which means you are less likely to get a job or have an education"); 
+ 
+});
+
+$(".learn").click(function(){
+
+    $(".learn2").text("You focus more on your games than on homwork or classwork, which makes you get bad grades"); 
+ 
+});
+
+$(".work").click(function(){
+
+    $(".work2").text("You are more focused on getting Tier 100 on Fortnite that you forget that you have work which causes you to lose income and v-bucks, it as well may cause you to get fired."); 
+ 
+});
